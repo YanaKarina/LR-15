6a9de34d45d96b99ef46dd0fb58ec9486c88ed71
YanaKarina <117731339+YanaKarina@users.noreply.github.com>
2023-02-14 18:33:41 +0300

    Add files via upload

---
+++ Задание 3.pas
@@ -0,0 +1,18 @@
+﻿Program zadanie3;
+var x,y: integer;
+function stepen (a,b: integer): integer;
+var n,k:integer;
+k:=1;
+n:=2;
+  begin
+   writeln(n);
+    n:=n*2;
+    k:=k+1
+  end;
+  begin
+    writeln('число?');
+    readln(x);
+    writeln('Степень?');
+    readln(y);
+    writeln(stepen(x,y));
+  end.
---
+++ Задание 6.pas
@@ -0,0 +1,10 @@
+﻿Program zadanie6;
+procedure fib (c,i,n:integer);
+begin
+  writeln (i+n,'');
+  if c=0 then Exit;
+    fib(c-1,i+n,i)
+end;
+begin
+  fib(0,1);
+end.
